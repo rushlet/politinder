ef3f5eae3f1bbbaa6ec69a90abb58ba9541f30b1
Becky Rush <becky.rush@bbc.co.uk>
2017-06-06 19:56:33 +0100

    fix not swiping cards

--- source/js/index.js
+++ source/js/index.js
@@ -2,10 +2,20 @@ import * as Swing from 'swing';
 import outputPolicies from './policies.js'
 import * as d3Request from 'd3-request';
 
-d3Request.json('../../../policies.json', ready);
+d3Request.json('../../../policies.json', function(error, policies) {
+    d3Request.json('../../../categories.json', function(error, categories) {
+        ready(error, {
+            policies: policies,
+            categories: categories
+        })
+    });
+});
 
 function ready(error, data) {
-    outputPolicies(error, data);
+    console.log('ready called');
+    console.log(data.policies, data.categories);
+    outputPolicies(error, data.policies, data.categories);
+    console.log(document.querySelectorAll('.policies-list li'));
     const cards = Array.from(document.querySelectorAll('.policies-list li'));
 
     console.log('cards', cards);
