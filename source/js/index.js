bf4fe8f1b75094a1b19dfd9dfd5559c98429ba3a
Luke Ewer <luke.ewer@bbc.co.uk>
2017-06-06 20:39:30 +0100

    Initial page styling

--- source/js/index.js
+++ source/js/index.js
@@ -5,20 +5,28 @@ import * as d3Request from 'd3-request';
 d3Request.json('../../../policies.json', ready);
 
 function ready(error, data) {
+
     outputPolicies(error, data);
     const cards = Array.from(document.querySelectorAll('.policies-list li'));
-
-    console.log('cards', cards);
     const stack = Swing.Stack();
-    console.log(stack);
-    cards.forEach((targetElement) => {
-      stack.createCard(targetElement);
-    });
+
+    // setup first two cards
+    createCard(stack, cards.shift());
+    createCard(stack, cards.shift());
 
     stack.on('throwout', function (e) {
         console.log(e.target.innerText || e.target.textContent, 'has been thrown out of the stack to the', e.throwDirection, 'direction.');
 
         e.target.classList.add('out-of-deck');
+        createCard(stack, cards.shift());
     });
 
 }
+
+function createCard(stack, el) {
+    if (el) {
+        stack.createCard(el, true);
+        el.classList.add('card--active');
+    }
+}
+
